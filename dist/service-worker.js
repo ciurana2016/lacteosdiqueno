ed7a75346758564b8b63d633c3ed0d2cdd001dd2
Victor Ciurana <ciurana2016@gmail.com>
2018-02-05 22:03:10 +0100

    BUG angular con pdf...

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","c6f396e0ee85755c818fa655c70b5ea3"],["inline.20917b2bcdbe0c75e889.bundle.js","d720223718b62af643a7efee5bfb1d2d"],["main.9396c050b20d78cc513d.bundle.js","98193cf24df0242b65fcf00155951720"],["polyfills.fcd51d693de42dfab15b.bundle.js","f266fbae4d6109d6c9c6f947d7953299"],["styles.cea11e4536aaea642b64.bundle.css","cea11e4536aaea642b64e0d8652408e6"],["vendor.db68cfc451659c07105e.bundle.js","711492bcdb911e3f888c4f4d3ad7879c"]];
+var precacheConfig = [["index.html","6c0a46a8a0a3923c3c30e1a18fece8ed"],["inline.276a789ee7f44307872b.bundle.js","77dc310d6cfd66e5df415ac7182e3c68"],["main.74b2ea4bf5a538ef6a68.bundle.js","4064e6def8ecda2ab42b45c93346a15f"],["polyfills.fcd51d693de42dfab15b.bundle.js","f266fbae4d6109d6c9c6f947d7953299"],["styles.cea11e4536aaea642b64.bundle.css","cea11e4536aaea642b64e0d8652408e6"],["vendor.db68cfc451659c07105e.bundle.js","711492bcdb911e3f888c4f4d3ad7879c"]];
 var cacheName = 'sw-precache-v3-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
